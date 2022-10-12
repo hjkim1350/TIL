29c6b3cee38f1de66f178a6d8c63784e7bd9aff1
hjkim1350 <hjkim1350@daum.net>
2022-10-12 17:54:06 +0900

    2022.10.12 업로드 추후 수정 예정

--- 06_Django/server_day7/articles/views.py
+++ 06_Django/server_day7/articles/views.py
@@ -1,11 +1,35 @@
+from nntplib import ArticleInfo
 from django.shortcuts import render, redirect
+
+from articles.forms import ArticleForm
+from .models import Article
 
 # Create your views here.
 def index(request):
-    return render(request, 'articles/index.html')
-
-def new(request):
-    return render(request, 'articles/new.html')
+    articles = Article.objects.order_by('-pk')
+    context = {
+        'articles': articles
+    }
+    return render(request, 'articles/index.html', context)
 
 def create(request):
-    return redirect('articles:index')
+    if request.method == "POST":
+        article_form = ArticleForm(request.POST)
+        if article_form.is_valid():
+            article_form.save()
+            return redirect('articles:index')
+    else:
+        article_form = ArticleForm()
+    
+    context = {
+        'article_form': article_form
+    }
+
+    return render(request, 'articles/new.html', context=context)
+
+def detail(request, pk):
+    article = Article.objects.get(pk=pk)
+    context = {
+        'article': article
+    }
+    return render(request, 'articles/detail.html', context)
